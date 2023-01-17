41c677fa018e150bd4eae96ab30bf6ad458a3fcc
trollsss <113868634+trollsss@users.noreply.github.com>
2023-01-17 11:45:48 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/GitHubService.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+interface GitHubService {
+
+    // GET /users/:username/repos
+    @GET("users/{username}/repos")
+    Call<List<Repos>> getRepos(@Path("username") String userName);
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
---
+++ app/src/main/java/com/example/myapplication/Repos.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Repos {
+
+    @SerializedName("name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
